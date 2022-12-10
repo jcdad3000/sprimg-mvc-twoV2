7b84050f4f7152eba35db4662dddf547e667c4b8
yjkim <31876648+jcdad3000@users.noreply.github.com>
2022-12-10 09:41:01 +0900

    챕터 1 타임리프 기본기능

--- src/main/java/hello/itemservice/domain/item/ItemType.java
+++ src/main/java/hello/itemservice/domain/item/ItemType.java
@@ -8,5 +8,9 @@ public enum ItemType {
     ItemType(String description) {
         this.description = description;
     }
+
+    public String getDescription() {
+        return description;
+    }
 }
 
